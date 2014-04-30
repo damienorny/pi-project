3af271f7cc5a278ff71bfe01692b75a661b60237
Damien <damienorny@gmail.com>
2014-04-30 13:27:53 +0200

    EDIT Modification du comportement du bonton Fire

--- index.php
+++ index.php
@@ -18,9 +18,16 @@
   </head>
   <body>
     
-  <h1>Canon électromagnétique <small>Contrôle</small></h1>
+  <!-- navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top alert navbarNotif" role="navigation" style="display:none">
+      <div class="container">
+        <span class="texteNotif"></span>
+      </div>
+    </nav>
+  <!-- navbar -->
+
+  <h1>Canon électromagnétique <small>Interface de contrôle</small></h1>
     <div class="well">
-      Interface de contrôle
       <div class="container">
         <div class="row">
           <div class="col-md-4 col-xs-4 col-md-offset-4 col-xs-offset-4"><button class="btn btn-primary btn-lg btn-block boutonHaut"><span class="glyphicon glyphicon-arrow-up"></span></button></div>
@@ -40,6 +47,29 @@
         </div>
       </div>
     </div>
+
+
+    <!-- Modal bootstrap pour mot de passe -->
+    <div class="modal fade" id="modalPassword" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+            <h4 class="modal-title" id="myModalLabel">Confirmez le mot de passe</h4>
+          </div>
+          <div class="modal-body">
+            <label>Mot de passe : <input type="password" id="MdP"></label>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+            <button type="button" class="btn btn-primary" id="confirmModal">Confirmer</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Fin modal bootstrap pour mot de passe -->
+
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-1.11.0.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
@@ -47,20 +77,51 @@
     <script type="text/javascript">
       $(document).ready(function() {
         $('.boutonHaut').click(function(event) {
-          $(this).attr('disabled', 'disabled');
+          
         });
         $('.boutonFire').click(function(event) {
-          if(!$('.boutonFire2').is(":visible"))
-          {
-            var MdP = prompt("Entrer le mot de passe pour pouvoir tirer :");
+            /** Décommenter pour activer la voix **
+            var voix = new SpeechSynthesisUtterance();
+            voix.lang = 'fr-FR';
+            voix.text = "Veuillez rentrer votre mot de passe";
+            speechSynthesis.speak(voix);*/
+
+            //var MdP = prompt("Entrer le mot de passe pour pouvoir tirer :");
+            if ($('.divBoutonFire2').is(":visible")) 
+            {
+              $('.divBoutonFire2').hide();
+            }
+            else
+            {
+              $('#modalPassword').modal();
+            }
+            
+            
+            
+        });
+        $('#confirmModal').click(function(event) {
+            $('#modalPassword').modal('hide');
+            var MdP = $('#MdP').val();
+            $('#MdP').val("");
             if(MdP != "azerty")
             {
-              alert("Mot de passe incorrect.")
+              $(".texteNotif").text("Mot de passe incorrect");
+              $(".navbarNotif").addClass('alert-danger');
+              $(".navbarNotif").show("slow");
+              setTimeout(function() {
+                $(".navbarNotif").hide("slow");
+                $(".navbarNotif").removeClass('alert-danger');
+              }, 3000);
               return;
             }
-            alert("Attention la mise à feu est désormais disponible.");
-          }
-            $('.divBoutonFire2').toggle();
+              $(".texteNotif").text("La mise à feu est désormais disponible");
+              $(".navbarNotif").addClass('alert-success');
+              $(".navbarNotif").show("slow");
+              setTimeout(function() {
+                $(".navbarNotif").hide("slow");
+                $(".navbarNotif").removeClass('alert-success');
+              }, 3000);
+              $('.divBoutonFire2').toggle();
         });
       });
     </script>
