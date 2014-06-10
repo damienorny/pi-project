8a59575223071fee302efb0e0bca4ffa803c350f
Mat2893 <mathieu.gerard@edu.esiee.fr>
2014-06-10 17:12:02 +0200

    EDIT fichier de controle avancé a tester

--- traitement.py
+++ traitement.py
@@ -3,25 +3,24 @@ import time
 import math
 import sys
 
-# if len(sys.argv) == 0:
-# 	couleur = Color.BLUE
-# elif sys.argv[1] == "rouge":
-# 	couleur = Color.RED
-# elif sys.argv[1] == "bleu":
-# 	couleur = Color.BLUE
-# elif sys.argv[1] == "jaune":
-# 	couleur = Color.YELLOW
-# elif sys.argv[1] == "vert":
-# 	couleur = Color.GREEN
+if len(sys.argv) == 0:
+	couleur = Color.BLUE
+elif sys.argv[1] == "rouge":
+	couleur = Color.RED
+elif sys.argv[1] == "bleu":
+	couleur = Color.BLUE
+elif sys.argv[1] == "jaune":
+	couleur = Color.YELLOW
+elif sys.argv[1] == "vert":
+	couleur = Color.GREEN
 
-# img = Image("test.jpg") 
-# color_distance = img.colorDistance(couleur).invert() 
-# blobs = color_distance.findBlobs() 
-# coord = blobs[-1].centroid() 
+img = Image("test.jpg") 
+color_distance = img.colorDistance(couleur).invert() 
+blobs = color_distance.findBlobs() 
+coord = blobs[-1].centroid() 
 
-# x = math.floor(coord[0])
+x = math.floor(coord[0])
 
-x = 264
 sys.exit(x)
 
 # points = [(x+10,y+10),(x+10,y-10),(x-10,y-10),(x-10,y+10)]
