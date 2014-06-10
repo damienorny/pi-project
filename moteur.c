8759766b87b7c6d7a8a23b87ef27ee0df136959d
Mat2893 <mathieu.gerard@edu.esiee.fr>
2014-06-10 17:05:59 +0200

    EDIT fichier de controle avancé

--- moteur.c
+++ moteur.c
@@ -3,22 +3,129 @@
 #include <unistd.h>
 #include "pifacedigital.h"
 
+int Coord(int Chgt);
+int Deplacement(int Bouge);
+
 int main( int argc, char *argv[] )
 {
-	int nbPas;
-	nbPas = atoi(argv[1]);
+	int nbPas, mode, verif;
+	mode = atoi(argv[1]);
 	int hw_addr = 0;
 	pifacedigital_open(hw_addr);
 	pifacedigital_enable_interrupts();
-	pifacedigital_write_reg(0xFF, OUTPUT, hw_addr);
+	pifacedigital_write_reg(0xF0, OUTPUT, hw_addr);
 
-		
+	if(mode == 1)
+	{
+		nbPas = atoi(argv[2]);
+		verif = Coord(7*nbPas);
+
+		if(verif == 0)
+		{
+			pifacedigital_write_reg(0x60, OUTPUT, hw_addr);
+			usleep(100000);
+			int i = 0;
+
+			if (nbPas > 0)
+			{
+				for (i; i < nbPas; i++)
+				{
+					pifacedigital_write_reg(0xC0, OUTPUT, hw_addr);
+					usleep(20000);
+					pifacedigital_write_reg(0x60, OUTPUT, hw_addr);
+					usleep(20000);
+					pifacedigital_write_reg(0x30, OUTPUT, hw_addr);
+					usleep(20000);
+					pifacedigital_write_reg(0x90, OUTPUT, hw_addr);
+					usleep(20000);
+				}
+			}
+			else
+			{
+				nbPas = abs(nbPas);
+				for (i; i < nbPas; i++)
+				{
+					pifacedigital_write_reg(0x90, OUTPUT, hw_addr);
+					usleep(20000);
+					pifacedigital_write_reg(0x30, OUTPUT, hw_addr);
+					usleep(20000);
+					pifacedigital_write_reg(0x60, OUTPUT, hw_addr);
+					usleep(20000);
+					pifacedigital_write_reg(0xC0, OUTPUT, hw_addr);
+					usleep(20000);
+				}
+			}
+			
+			pifacedigital_write_reg(0xF0, OUTPUT, hw_addr);
+		}
+	}
+	else if(mode == 2)
+	{
+		//system("CAPTURE")
+		int mvt;
+		mvt = system("python traitement.py");
+		if (mvt < 330 || mvt > 310)
+		{
+			system("./scripts/test")
+		}
+		else
+		{
+			int Correction;
+			char CorrectionC[50];
+			Correction = mvt - 320;
+			Correction = Correction / 66;
+			verif = Coord(7*Correction);
+
+			if (verif == 0)
+			{
+				Deplacement(Correction);
+				system("./scripts/moteur 2");
+			}
+		}
+	}
+	return 0;
+}
+
+int Coord(int Chgt)
+{
+	FILE* fichier = NULL;
+	char chaine[5] = "";
+	float NCoord = 0; 
+	fichier = fopen("Coordonnees.txt", "r+");
+
+	if (fichier == NULL)
+    {
+    	printf("Impossible d'ouvrir le fichier texte");
+    }
+
+    fgets(chaine, 3, fichier);
+    fclose(fichier);
+
+    NCoord = atoi(chaine);
+    NCoord = NCoord + Chgt;
+
+    if(NCoord < 0 || NCoord > 180)
+    {
+    	return -1;
+    }
+    else
+    {
+    	fichier = fopen("Coordonnees.txt", "w+");
+	    fprintf(fichier, "%d",NCoord);
+	    fclose(fichier);
+		return 0;
+    }
+}
+
+int Deplacement(int Bouge)
+{
 	pifacedigital_write_reg(0x60, OUTPUT, hw_addr);
 	usleep(100000);
 	int i = 0;
-	if (nbPas > 0)
+
+	if (Bouge > 0)
 	{
-		for (i; i < nbPas; i++)
+		for (i; i < Bouge; i++)
 		{
 			pifacedigital_write_reg(0xC0, OUTPUT, hw_addr);
 			usleep(20000);
@@ -32,8 +139,8 @@ int main( int argc, char *argv[] )
 	}
 	else
 	{
-		nbPas = abs(nbPas);
-		for (i; i < nbPas; i++)
+		Bouge = abs(Bouge);
+		for (i; i < Bouge; i++)
 		{
 			pifacedigital_write_reg(0x90, OUTPUT, hw_addr);
 			usleep(20000);
@@ -47,5 +154,4 @@ int main( int argc, char *argv[] )
 	}
 	
 	pifacedigital_write_reg(0xF0, OUTPUT, hw_addr);
-	return 0;
 }
--- traitement.py
+++ traitement.py
@@ -2,22 +2,29 @@ from SimpleCV import Color, Image
 import time
 import math
 import sys
-if len(sys.argv) == 0:
-	couleur = Color.BLUE
-elif sys.argv[1] == "rouge":
-	couleur = Color.RED
-elif sys.argv[1] == "bleu":
-	couleur = Color.BLUE
-img = Image("test.jpg") 
-color_distance = img.colorDistance(couleur).invert() 
-blobs = color_distance.findBlobs() 
-# blobs.draw(color=Color.PUCE, width=3)
-# img.addDrawingLayer(color_distance.dl())
-coord = blobs[-1].centroid() 
-print(coord)
-x = math.floor(coord[0])
-y = math.floor(coord[1])
-points = [(x+10,y+10),(x+10,y-10),(x-10,y-10),(x-10,y+10)]
-img.dl().polygon(points, filled=True, color=Color.RED)
-img.show()
-time.sleep(10)
+
+# if len(sys.argv) == 0:
+# 	couleur = Color.BLUE
+# elif sys.argv[1] == "rouge":
+# 	couleur = Color.RED
+# elif sys.argv[1] == "bleu":
+# 	couleur = Color.BLUE
+# elif sys.argv[1] == "jaune":
+# 	couleur = Color.YELLOW
+# elif sys.argv[1] == "vert":
+# 	couleur = Color.GREEN
+
+# img = Image("test.jpg") 
+# color_distance = img.colorDistance(couleur).invert() 
+# blobs = color_distance.findBlobs() 
+# coord = blobs[-1].centroid() 
+
+# x = math.floor(coord[0])
+
+x = 264
+sys.exit(x)
+
+# points = [(x+10,y+10),(x+10,y-10),(x-10,y-10),(x-10,y+10)]
+# img.dl().polygon(points, filled=True, color=Color.RED)
+# img.show()
+#time.sleep(10)
