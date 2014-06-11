f6f627658bd87b469f73a10a647dbe8541c435c6
Mat2893 <mathieu.gerard@edu.esiee.fr>
2014-06-11 10:48:14 +0200

    EDIT fichier de controle avancé a tester avec retour après tir

--- moteur.c
+++ moteur.c
@@ -23,41 +23,7 @@ int main( int argc, char *argv[] )
 
 		if(verif == 0)
 		{
-			pifacedigital_write_reg(0x60, OUTPUT, hw_addr);
-			usleep(100000);
-			int i = 0;
-
-			if (nbPas > 0)
-			{
-				for (i; i < nbPas; i++)
-				{
-					pifacedigital_write_reg(0xC0, OUTPUT, hw_addr);
-					usleep(20000);
-					pifacedigital_write_reg(0x60, OUTPUT, hw_addr);
-					usleep(20000);
-					pifacedigital_write_reg(0x30, OUTPUT, hw_addr);
-					usleep(20000);
-					pifacedigital_write_reg(0x90, OUTPUT, hw_addr);
-					usleep(20000);
-				}
-			}
-			else
-			{
-				nbPas = abs(nbPas);
-				for (i; i < nbPas; i++)
-				{
-					pifacedigital_write_reg(0x90, OUTPUT, hw_addr);
-					usleep(20000);
-					pifacedigital_write_reg(0x30, OUTPUT, hw_addr);
-					usleep(20000);
-					pifacedigital_write_reg(0x60, OUTPUT, hw_addr);
-					usleep(20000);
-					pifacedigital_write_reg(0xC0, OUTPUT, hw_addr);
-					usleep(20000);
-				}
-			}
-			
-			pifacedigital_write_reg(0xF0, OUTPUT, hw_addr);
+			Deplacement(nbPas);
 		}
 	}
 	else if(mode == 2)
